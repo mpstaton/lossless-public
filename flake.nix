0e773029ae1002bac90ce5d4dd7729f6aed0943b
mpstaton <mpstaton@gmail.com>
2024-12-27 20:19:52 +0400

    ran nix flake check

--- flake.nix
+++ flake.nix
@@ -1,53 +1,28 @@
+# flake.nix "flake" "imports"
 {
-  description = "202501_Astro--01";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs";
 
-    /*
-    Where flake parts are declared
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-    */
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
 
-    devShell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Development
 
-    /*
-    Where the shell is declared
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    */
   };
+  
+  outputs = inputs@{ self, nixpkgs, flake-parts, devshell }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-  outputs = { self, nixpkgs, flake-parts, flake-utils, devshell }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in with pkgs; rec {
-        # Development environment
-        devShell = mkShell {
-          name = "202501_Astro--01";
-          nativeBuildInputs = [ poetry ];
-        };
+    imports = [];
 
-        # Runtime package
-        packages.app = poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-        };
-
-        # The default package when a specific package name isn't specified.
-        defaultPackage = packages.app;
-      }
-    );
+    flake = {
+      # your existing definitions before using flake-parts...
+    };
+  };
 }
