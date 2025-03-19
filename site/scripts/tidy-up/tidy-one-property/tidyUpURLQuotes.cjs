ec700d9b6135e2142425d2bb37afc52a16a5de11
Michael Staton <mpstaton@gmail.com>
2025-03-19 02:12:34 -0500

    missed two

--- site/scripts/tidy-up/tidy-one-property/tidyUpURLQuotes.cjs
+++
@@ -1,4 +0,0 @@
-import { USER_OPTIONS } from '../getUserOptions.cjs';
-import { knownErrorCases, correctionFunctions } from '../../build-scripts/getKnownErrorsAndFixes.cjs';
-
-URL_PROPERTIES = USER_OPTIONS.frontmatterPropertySets.urlProperties;
